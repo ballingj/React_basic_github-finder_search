110e584f81a34645dca441ee4c0301604e51d4b6
Jeff Ballinger <kumite@Jeffs-MBP.localdomain>
2021-08-05 19:06:58 -0500

    github clientID and clientSecret

--- src/archived/App5.js
+++ src/archived/App5.js
@@ -1,4 +1,3 @@
-//added the github clientID clientSecret codes
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -14,7 +13,7 @@ class App extends Component {
   async componentDidMount() {
     this.setState({ loading: true });
 
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}{process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const res = await axios.get('https://api.github.com/users');
     
     this.setState({ users: res.data, loading: false });
   }
