8b445e98bb10705e023a1d37aaaae6267e9861a8
Jeff Ballinger <kumite@Jeffs-MBP.localdomain>
2021-08-08 20:12:56 -0500

    added Clearing features

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class Search extends Component {
   state = {
     text: ''
   }
   
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+  }
+
   onSubmit = (e) => {
     e.preventDefault();  //prevents the default behavior of the submit 
     this.props.searchUsers(this.state.text);    // this is a function that we call up the chain App.js
@@ -13,16 +20,25 @@ export class Search extends Component {
 
   onChange = (e) => {
     //this.setState({ text: e.target.value }) //e is for event -target.value is the content of the input
-    this.setState({ [e.target.name]: e.target.value }) //an improvement over avobe code
+    this.setState({ [e.target.name]: e.target.value }) //an improvement over avobe code [] is necessary for javascript to interpret the value as key and not just another string
   }
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form className="form" onSubmit={this.onSubmit}>
           <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange }/>
           <input type="submit" value="Search" className="btn btn-dark btn-block" />
         </form>
+        {showClear && (
+          <button
+            className="btn btn-light btn-block"
+            onClick={clearUsers}
+          >
+            Clear
+          </button>
+        )}
       </div>
     )
   }
