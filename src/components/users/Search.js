6d35d0f8b7b6df3462980a7a87811055ad7d9e81
Jeff Ballinger <kumite@Jeffs-MBP.localdomain>
2021-08-20 11:36:48 -0500

    Search component converted to Hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,52 +1,36 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react';
 
-export class Search extends Component {
-  state = {
-    text: ''
-  }
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+  const [text, setText] = useState('');
   
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlart: PropTypes.func,
-  }
-
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();  //prevents the default behavior of the submit
-    if (this.state.text === '') {
-      this.props.setAlert('Please enter a search term', 'light');
+    if (text === '') {
+      setAlert('Please enter a search term', 'light');
     } else {
-      this.props.searchUsers(this.state.text);    // this is a function that we call up the chain App.js
-      this.setState({ text: '' })   // clear the text in the form after
+      searchUsers(text)    // this is a function that we call up the chain App.js
+      setText('')         // clear the text in the form after
     }
   };
 
-  onChange = (e) => {
-    //this.setState({ text: e.target.value }) //e is for event -target.value is the content of the input
-    this.setState({ [e.target.name]: e.target.value }) //an improvement over avobe code [] is necessary for javascript to interpret the value as key and not just another string
-  }
+  const onChange = (e) => setText(e.target.value);  // refactored to use useState
 
-  render() {
-    const { showClear, clearUsers } = this.props;
-    return (
-      <div>
-        <form className="form" onSubmit={this.onSubmit}>
-          <input type="text" name="text" placeholder="Search User..." value={this.state.text} onChange={this.onChange }/>
-          <input type="submit" value="Search" className="btn btn-dark btn-block" />
-        </form>
-        {showClear && (
-          <button
-            className="btn btn-light btn-block"
-            onClick={clearUsers}
-          >
-            Clear
-          </button>
-        )}
-      </div>
-    )
-  }
+  return (
+    <div>
+      <form className="form" onSubmit={onSubmit}>
+        <input type="text" name="text" placeholder="Search Users..." value={text} onChange={onChange}/>
+        <input type="submit" value="Search" className="btn btn-dark btn-block" />
+      </form>
+      {showClear && (
+        <button
+          className="btn btn-light btn-block"
+          onClick={clearUsers}
+        >
+          Clear
+        </button>
+      )}
+    </div>
+  )
 }
 
 export default Search
